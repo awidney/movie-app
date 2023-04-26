390be55599d859267d2bd5bfd616551afff1169f
Adam Widney <awidney@my.bcit.ca>
2023-04-26 15:42:02 -0700

    Add md, xl, 2xl media query styles

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -14,10 +14,10 @@ function Trending() {
   });
 
   return (
-    <section>
+    <section className='mx-auto my-8 xl:w-[65rem] 2xl:w-[85rem]'>
       <h2>Trending</h2>
       <div className='h-scroll'>
-        <div className='flex min-w-max gap-4 pb-2'>
+        <div className='flex min-w-max gap-4 py-2 md:gap-8'>
           {trendingMovies?.results.slice(0, 10).map((movie) => (
             <CardTrending
               key={movie.id}
