ae8d0eaed74ec14c3e7ac26febc306040c8cc057
Adam Widney <awidney@my.bcit.ca>
2023-04-26 15:40:09 -0700

    Add md media query styles

--- src/components/CardTrending.jsx
+++ src/components/CardTrending.jsx
@@ -1,11 +1,11 @@
 import RatingBar from './RatingBar';
 
 function CardTrending({ poster, title, releaseDate, rating }) {
-  const posterUrl = `https://image.tmdb.org/t/p/w300${poster}`;
+  const posterUrl = `https://image.tmdb.org/t/p/w780${poster}`;
 
   return (
-    <div className='relative my-2'>
-      <div className='h-[140px] w-[240px]'>
+    <div className='relative my-2 cursor-pointer'>
+      <div className='h-[140px] w-[240px]  md:h-[230px] md:w-[470px]'>
         <img
           className='h-full w-full rounded-lg object-cover opacity-60'
           src={posterUrl}
@@ -13,8 +13,8 @@ function CardTrending({ poster, title, releaseDate, rating }) {
         />
       </div>
       <div className='absolute bottom-0 mb-2 ml-4'>
-        <p className='font-Poppins text-xs'>{releaseDate}</p>
-        <h3 className='max-w-[240px] font-Poppins text-sm font-bold'>
+        <p className='font-Poppins text-xs md:text-base'>{releaseDate}</p>
+        <h3 className='max-w-[240px] font-Poppins text-sm font-bold md:max-w-[470px] md:text-xl'>
           {title}
         </h3>
       </div>
