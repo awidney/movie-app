32d0ac830db9160ebd405a89975f5237cec3511b
Nate <nateaba@gmail.com>
2023-04-25 20:46:00 -0700

    Add Nav Tablet and Desktop Style Queries

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -26,49 +26,50 @@ function Nav() {
   };
 
   return (
-    <nav className='w-100 pt-2'>
+    <nav className='w-100 pt-2 xl:pt-4'>
       <div
-        className={`flex items-center justify-between pr-1.5 ${
+        className={`flex items-center justify-between pr-1.5 xl:justify-around ${
           searchActive ? 'hidden' : ''
         }`}
       >
         <a href='/'>
           <img
-            className='h-16 w-16'
+            className='h-16 w-16 xl:h-20 xl:w-20'
             src='./public/nafilms4.svg'
             alt='NA Films Logo'
           />
         </a>
 
         <form
-          className=' my-3 flex hidden h-10 items-center gap-2 pr-1.5'
+          className='-webkit-backdrop-blur-lg my-3 hidden h-10 items-center gap-2 rounded-lg border border-white border-opacity-10 bg-opacity-20 pr-1.5 shadow-md backdrop-blur-lg md:inline-flex md:w-[55%] xl:w-[560px] xl:py-6'
           ref={searchRef}
         >
+          <button type='submit' className='pl-4'>
+            {' '}
+            <img
+              className='h-6 w-6'
+              src='./public/search2.svg'
+              alt='Search button'
+            />
+          </button>
+
           <input
             type='text'
             placeholder='Search for movies...'
             className=' ml-3 flex-grow bg-transparent font-Poppins text-white focus:outline-none'
           />
-          <button type='submit'>
-            {' '}
-            <img
-              className='h-10 w-10'
-              src='./public/search2.svg'
-              alt='Search button'
-            />
-          </button>
         </form>
 
         <div className='flex gap-2'>
           <a href='/'>
             <img
-              className='h-10 w-10'
+              className='hover:fill h-10 w-10 xl:h-12 xl:w-12'
               src='./public/bookmark-add.svg'
               alt='Bookmarks Link'
             />
           </a>
 
-          <button onClick={toggleSearch}>
+          <button onClick={toggleSearch} className='md:hidden'>
             <img
               className='h-10 w-10'
               src='./public/search2.svg'
