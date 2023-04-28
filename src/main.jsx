22318b631e52c55d5c52a06f6d4a2560d416704e
Nate <nateaba@gmail.com>
2023-04-28 11:24:45 -0700

    Add browser router and new pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,17 @@ import App from './components/App.jsx';
 import './styles/index.css';
 import { QueryClientProvider, QueryClient } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
