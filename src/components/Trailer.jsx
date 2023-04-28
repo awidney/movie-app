57c6bba353e1affbfbc215a2e6a5cb21dd446e6f
Adam Widney <awidney@my.bcit.ca>
2023-04-27 19:05:11 -0700

    Add Trailer component

---
+++ src/components/Trailer.jsx
@@ -0,0 +1,15 @@
+function Trailer({ trailerKey }) {
+  return (
+    <div className='relative my-4 h-0 w-full pb-[56.25%]'>
+      <iframe
+        className='absolute left-0 top-0 h-full w-full'
+        src={`https://www.youtube.com/embed/${trailerKey}`}
+        title='YouTube video player'
+        allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share'
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+}
+
+export default Trailer;
