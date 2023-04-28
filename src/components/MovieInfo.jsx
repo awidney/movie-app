401a4522beb6b8a144165f20f10016d45c5bd175
Adam Widney <awidney@my.bcit.ca>
2023-04-28 13:56:12 -0700

    Use grid for layout, import SinglePoster, add media queries

--- src/components/MovieInfo.jsx
+++ src/components/MovieInfo.jsx
@@ -1,18 +1,16 @@
 import { useQuery } from 'react-query';
 import axios from 'axios';
-import RatingBar from './RatingBar';
 import Cast from './Cast';
 import Trailer from './Trailer';
 import Genres from './Genres';
+import SinglePoster from './SinglePoster';
 
 function MovieInfo() {
-  const posterUrl = 'https://image.tmdb.org/t/p/w780';
-
   const { data: movieInfo } = useQuery({
     queryKey: 'movieInfo',
     queryFn: async () => {
       const response = await axios.get(
-        'https://api.themoviedb.org/3/movie/436270?api_key=e1eb6a4fd746d268382a20cd605740a8&append_to_response=videos,credits'
+        'https://api.themoviedb.org/3/movie/324857?api_key=e1eb6a4fd746d268382a20cd605740a8&append_to_response=videos,credits'
       );
       return response.data;
     },
@@ -33,38 +31,32 @@ function MovieInfo() {
 
   return (
     <div className='relative'>
-      <section>
-        <div className='flex justify-between gap-4'>
-          <h1 className='font-Inter text-2xl'>{movieInfo?.title}</h1>
-          <img
-            className='h-8 w-8'
-            src='../../bookmark-add.svg'
-            alt='Add to favorites button'
-          />
-        </div>
-        <div className='flex gap-4 '>
-          <p>{movieInfo?.release_date.slice(0, 4)}</p>
-          <p>{`${hours}h ${minutes}m`}</p>
-        </div>
+      <div className='flex justify-between gap-4'>
+        <h1 className='font-Inter text-2xl md:text-4xl'>{movieInfo?.title}</h1>
+        <img
+          className='h-8 w-8'
+          src='../../bookmark-add.svg'
+          alt='Add to favorites button'
+        />
+      </div>
+
+      <div className='mt-2 flex gap-4 lg:text-xl'>
+        <p>{movieInfo?.release_date.slice(0, 4)}</p>
+        <p>{`${hours}h ${minutes}m`}</p>
+      </div>
+
+      <div className='mb-16 grid gap-4 sm:grid-cols-[1fr_3fr]'>
         <Trailer trailerKey={trailerKey} />
-        <div className='my-12 flex gap-4'>
-          <div className='relative h-[180px] w-[120px] flex-shrink-0'>
-            <img
-              className='h-full w-full rounded-lg object-cover'
-              src={`${posterUrl}${movieInfo?.poster_path}`}
-              alt={movieInfo?.title}
-            />
-            <RatingBar rating={movieInfo?.vote_average.toFixed(1) * 10} />
-          </div>
-          <div className='w-30 flex flex-col gap-4'>
-            <Genres movieInfo={movieInfo} />
-            <p className='flex-1 text-sm'>{movieInfo?.overview}</p>
-          </div>
+        <SinglePoster movieInfo={movieInfo} />
+        <div className='w-30 flex flex-col gap-4 sm:col-span-2'>
+          <Genres movieInfo={movieInfo} />
+          <p className='flex-1 text-sm lg:text-base'>{movieInfo?.overview}</p>
         </div>
-      </section>
+      </div>
+
       <section>
         <h2>Top Billed Cast</h2>
-        <div className='h-scroll my-4 flex gap-4 pb-4 pt-2'>
+        <div className='h-scroll my-4 flex gap-4 overflow-x-scroll pb-4 pt-2'>
           {movieInfo?.credits.cast.slice(0, 10).map((cast) => (
             <Cast
               key={cast.id}
