3c054ec7506c6ac66dca7eb68f8a099f5b0b44d6
Nate <nateaba@gmail.com>
2023-04-29 15:32:10 -0700

    Add remaining category page files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Nav from './Nav';
 import Footer from './Footer';
-import PageHome from '../pages/PageHome.jsx';
-import PageTrending from '../pages/PageTrending';
+import Home from '../pages/Home.jsx';
+import Trending from '../pages/Trending';
 import MovieInfo from '../pages/MovieInfo';
 import ScrollToTop from './ScrollToTop';
+import Popular from '../pages/Popular';
+import TopRated from '../pages/TopRated';
+import NowPlaying from '../pages/NowPlaying';
+import Upcoming from '../pages/Upcoming';
 
 function App() {
   return (
@@ -13,8 +17,12 @@ function App() {
       <ScrollToTop />
       <main className='mx-auto my-8 px-4 lg:w-[1024px] xl:w-[67.5rem] 2xl:w-[87rem]'>
         <Routes>
-          <Route path='/' exact element={<PageHome />} />
-          <Route path='/movie/trending' element={<PageTrending />} />
+          <Route path='/' exact element={<Home />} />
+          <Route path='/movie/trending' element={<Trending />} />
+          <Route path='/movie/popular' element={<Popular />} />
+          <Route path='/movie/top-rated' element={<TopRated />} />
+          <Route path='/movie/now-playing' element={<NowPlaying />} />
+          <Route path='/movie/upcoming' element={<Upcoming />} />
           <Route path='/movie/:id/:title' element={<MovieInfo />} />
         </Routes>
       </main>
