d0c8118a13ffdaab3f3a18356b24c8922db7ae0a
Adam Widney <awidney@my.bcit.ca>
2023-04-29 11:18:49 -0700

    Add <Link> for movie poster, title

--- src/components/CardTrending.jsx
+++ src/components/CardTrending.jsx
@@ -1,25 +1,33 @@
 import RatingBar from './RatingBar';
+import { Link } from 'react-router-dom';
 
-function CardTrending({ poster, title, releaseDate, rating }) {
+function CardTrending({ poster, title, releaseDate, rating, id }) {
   const posterUrl = `https://image.tmdb.org/t/p/w780${poster}`;
 
+  const formattedUrlTitle = title
+    .toLowerCase()
+    .replace(/[^a-zA-Z0-9\s]/g, '') // remove non-alphanumeric characters excluding spaces
+    .replace(/\s+/g, '-'); // replace spaces with dashes
+
   return (
-    <div className='relative my-2 cursor-pointer'>
-      <div className='h-[140px] w-[240px]  md:h-[230px] md:w-[470px]'>
-        <img
-          className='h-full w-full rounded-lg object-cover opacity-60'
-          src={posterUrl}
-          alt={title}
-        />
+    <Link to={`/movie/${id}/${formattedUrlTitle}`}>
+      <div className='relative my-2 cursor-pointer'>
+        <div className='h-[140px] w-[240px]  md:h-[230px] md:w-[470px]'>
+          <img
+            className='h-full w-full rounded-lg object-cover opacity-60'
+            src={posterUrl}
+            alt={title}
+          />
+        </div>
+        <div className='absolute bottom-0 mb-2 ml-4'>
+          <p className='font-Poppins text-xs md:text-base'>{releaseDate}</p>
+          <h3 className='font-Inter text-sm font-bold md:max-w-[470px] md:text-xl'>
+            {title}
+          </h3>
+        </div>
+        <RatingBar rating={rating} />
       </div>
-      <div className='absolute bottom-0 mb-2 ml-4'>
-        <p className='font-Poppins text-xs md:text-base'>{releaseDate}</p>
-        <h3 className='font-Inter text-sm font-bold md:max-w-[470px] md:text-xl'>
-          {title}
-        </h3>
-      </div>
-      <RatingBar rating={rating} />
-    </div>
+    </Link>
   );
 }
 
