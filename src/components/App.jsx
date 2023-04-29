0def77c0499b7a4d6a47c3a2ea40d2886bae540d
Adam Widney <awidney@my.bcit.ca>
2023-04-29 11:48:02 -0700

    Add ScrollToTop.jsx to reset scroll position on pathname change

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,13 @@ import Footer from './Footer';
 import PageHome from '../pages/PageHome.jsx';
 import PageTrending from '../pages/PageTrending';
 import MovieInfo from '../pages/MovieInfo';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
   return (
     <div>
       <Nav />
+      <ScrollToTop />
       <main className='mx-auto my-8 px-4 lg:w-[1024px] xl:w-[67.5rem] 2xl:w-[87rem]'>
         <Routes>
           <Route path='/' element={<PageHome />} />
