e8fd8e1186274b3ef49c6a8b8f8b2f5666578388
Nate <nateaba@gmail.com>
2023-04-29 22:06:43 -0700

    Add about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
 function About() {
-  return <div>Test</div>;
+  return (
+    <section>
+      <h2>About</h2>
+      <p className='py-2'>Hello</p>
+    </section>
+  );
 }
 
 export default About;
