a3e6df1a79ef31f2348fa188aa98cd3938641307
Nate <nateaba@gmail.com>
2023-04-30 21:30:30 -0700

    Add "NR" value to movies that are not rated

--- src/components/RatingBar.jsx
+++ src/components/RatingBar.jsx
@@ -2,12 +2,14 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function RatingBar({ rating }) {
+  const value = rating === 0 ? 'N/A' : rating / 100;
+
   return (
-    <div className='absolute right-2 top-2 w-10 md:w-12'>
+    <div className='absolute right-2 top-2 w-8 md:w-12'>
       <CircularProgressbar
-        value={rating / 100}
+        value={value}
         maxValue={1}
-        text={`${rating}%`}
+        text={rating === 0 ? 'NR' : `${rating}%`}
         background
         styles={buildStyles({
           textColor: '#fff',
