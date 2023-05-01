eafe12439ae3fd44e184f1c5a19f48df97213d87
Nate <nateaba@gmail.com>
2023-04-30 19:05:39 -0700

    Center about page styles

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,28 +3,26 @@ import React from 'react';
 function About() {
   return (
     <section>
-      <h2>About</h2>
-      <div className='justify-between gap-6 md:flex'>
-        <p className='max-w-xl py-2'>
-          N/A Films is an online movie database that lets you track your
-          favorite films. Browse our collection of trending, popular, top-rated,
-          upcoming, and now playing titles, and get detailed information on each
-          movie, including cast, and plot summaries. Our user-friendly platform
-          is designed for movie enthusiasts, and we are committed to delivering
-          reliable and accurate information. Join us today and discover a new
-          world of movie entertainment.
-        </p>
-        <br />
-        <p className='md:max-w-md md:text-right'>
-          *This product uses the TMDb API but is not endorsed or certified by
-          TMDb.
-          <img
-            className='mx-auto mt-5 w-16'
-            src='/tmdbicon.svg'
-            alt='TMDB Icon'
-          />
-        </p>
-      </div>
+      <h2 className='text-center'>About</h2>
+      <p className='mx-auto max-w-xl py-2 text-center'>
+        N/A Films is an online movie database that lets you track your favorite
+        films. Browse our collection of trending, popular, top-rated, upcoming,
+        and now playing titles, and get detailed information on each movie,
+        including cast, and plot summaries. Our user-friendly platform is
+        designed for movie enthusiasts, and we are committed to delivering
+        reliable and accurate information. Join us today and discover a new
+        world of movie entertainment.
+      </p>
+      <br />
+      <p className='mx-auto text-center'>
+        *This product uses the TMDb API but is not endorsed or certified by
+        TMDb.
+        <img
+          className='mx-auto mt-5 w-16'
+          src='/tmdbicon.svg'
+          alt='TMDB Icon'
+        />
+      </p>
     </section>
   );
 }
