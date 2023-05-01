7a0f9937181c9ddc926e0ea2817d51b945f7935f
Nate <nateaba@gmail.com>
2023-04-30 21:37:30 -0700

    Edit rating bar logic for not rated movies

--- src/components/RatingBar.jsx
+++ src/components/RatingBar.jsx
@@ -2,12 +2,10 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
 function RatingBar({ rating }) {
-  const value = rating === 0 ? 'N/A' : rating / 100;
-
   return (
     <div className='absolute right-2 top-2 w-8 md:w-12'>
       <CircularProgressbar
-        value={value}
+        value={rating ? rating / 100 : 0}
         maxValue={1}
         text={rating === 0 ? 'NR' : `${rating}%`}
         background
